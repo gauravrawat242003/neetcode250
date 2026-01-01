1a17f5a62254ad6cb2bfd3aa6a5d7aa88af9a68b
Gaurav <gauravrawat7945@gmail.com>
2026-01-02 00:52:08 +0530

    selMatII

---
+++ binarysearch/searchElMatrixII.java
@@ -0,0 +1,36 @@
+package binarysearch;
+
+public class searchElMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int row = 0 , col = m-1;
+
+        while(row < n && col >= 0){
+            if(matrix[row][col] == target){
+                return true;
+            }else if(matrix[row][col] < target){
+                row++;
+            }else col--;
+        }
+        return false;
+}
+
+    public static void main(String[] args) {
+        searchElMatrixII sol = new searchElMatrixII();
+
+        int[][] matrix = {
+            {1, 4, 7, 11, 15},
+            {2, 5, 8, 12, 19},
+            {3, 6, 9, 16, 22},
+            {10, 13, 14, 17, 24},
+            {18, 21, 23, 26, 30}
+        };
+        int target = 5;
+
+        boolean ans = sol.searchMatrix(matrix, target);
+
+        System.out.println(ans);
+    }
+}
